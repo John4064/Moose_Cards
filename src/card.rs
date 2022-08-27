f3133baebaee00e3032d0e73c4a136fb92774ef0
John Parkhurst <jparkhurst120@gmail.com>
2022-08-27 00:18:17 -0400

    Need to setup inheritance
    Object Structure sucks, look into oop for rust and refactor so unit and item cards are subclasses of class

--- src/card.rs
+++ src/card.rs
@@ -4,17 +4,24 @@ use serde::{Serialize, Deserialize};
 #[derive(Deserialize, Serialize, Debug)]
 pub struct UnitCard {
     id: i32,
+    card_type: String,
     name: String,
+    description: String,
     card_stats: Stats
 }
-
-
-
-
 
 #[derive(Deserialize, Serialize, Debug)]
 struct Stats{
     hp: i32,
     power: i32,
     speed: i32
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+struct ItemCard{
+    id: i32,
+    item_type: String,
+    item_name: String,
+    item_description : String,
+    item_stats: Stats
 }
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ pub mod read_file;
 pub mod card;
 
 fn main() {
-    let temp_unit:UnitCard = read_file::read_f("moose.json".to_string());
+    let temp_unit:UnitCard = read_file::read_f("items/dagger.json".to_string());
 
 
-    println!("{}",temp_unit.borrow("id"))
+    println!("{}",3)
 }
